0ef872898a85708a33f4f8e6c1510d24cd2eec2a
algorythmist <dimitri@tecacet.com>
2021-07-13 09:58:47 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='quantum-computing',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/algorythmist/quantum-computing',
+    license='MIT',
+    author='Dimitri Papaioannou',
+    author_email='dimitri@tecacet.com',
+    description='Quantum Computing Simulations',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3'
+)
